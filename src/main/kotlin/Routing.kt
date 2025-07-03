2bab67932d3ba0f7f539b259eb79c3e45778c8f1
doanvu20122000 <doanvu20122000@gmail.com>
2025-07-03 17:50:56 +0700

    modify

--- src/main/kotlin/Routing.kt
+++ src/main/kotlin/Routing.kt
@@ -1,10 +1,11 @@
 package com.jin
 
+import com.jin.models.BaseResponse
 import com.jin.repository.UserRepo
 import io.ktor.http.*
 import io.ktor.server.application.*
-import io.ktor.server.request.receive
-import io.ktor.server.response.*
+import io.ktor.server.request.*
+import io.ktor.server.response.respond
 import io.ktor.server.routing.*
 import models.User
 
@@ -13,37 +14,104 @@ fun Application.configureRouting() {
 
 fun Route.userRoutes(repo: UserRepo) {
     route("/users") {
-        get { call.respond(repo.all()) }
+        get {
+            val users = repo.all()
+            call.respondBase(HttpStatusCode.OK, "Lấy danh sách thành công", users)
+        }
         get("{id}") {
             val id = call.parameters["id"]?.toLongOrNull()
-            if (id == null) return@get call.respond(HttpStatusCode.BadRequest, "ID không hợp lệ")
-            repo.findById(id)?.let { call.respond(it) }
-                ?: call.respond(HttpStatusCode.NotFound, "Không tìm thấy user")
+            if (id == null) {
+                call.respondBase(HttpStatusCode.BadRequest, "ID không hợp lệ", Unit)
+                return@get
+            }
+
+            val user = repo.findById(id)
+            if (user != null) {
+                call.respondBase(
+                    HttpStatusCode.OK,
+                    "Lấy user thành công",
+                    user
+                )
+            } else {
+                call.respondBase(
+                    HttpStatusCode.OK,
+                    "Không tìm thấy user với id = $id",
+                    Unit
+                )
+            }
         }
 
         post {
-            val user = call.receive<User>()
-            val created = repo.create(user)
-            call.respond(HttpStatusCode.Created, created)
+            val userReq = call.receive<User>()
+            val created = repo.create(userReq)
+            call.respondBase(
+                HttpStatusCode.Created,
+                "Tạo user thành công",
+                created
+            )
         }
 
         put("{id}") {
             val id = call.parameters["id"]?.toLongOrNull()
-            if (id == null) return@put call.respond(HttpStatusCode.BadRequest, "ID không hợp lệ")
-            val user = call.receive<User>()
-            if (repo.update(id, user))
-                call.respond(HttpStatusCode.OK, "Cập nhật thành công")
-            else
-                call.respond(HttpStatusCode.NotFound, "Không tìm thấy user")
+            if (id == null) {
+                call.respondBase(HttpStatusCode.BadRequest, "ID không hợp lệ", Unit)
+                return@put
+            }
+            val userReq = call.receive<User>()
+            if (repo.update(id, userReq)) {
+                call.respondBase(
+                    HttpStatusCode.OK,
+                    "Cập nhật thành công",
+                    Unit
+                )
+            } else {
+                call.respondBase(
+                    HttpStatusCode.NotFound,
+                    "Không tìm thấy user với id = $id",
+                    Unit
+                )
+            }
         }
 
         delete("{id}") {
             val id = call.parameters["id"]?.toLongOrNull()
-            if (id == null) return@delete call.respond(HttpStatusCode.BadRequest, "ID không hợp lệ")
-            if (repo.delete(id))
-                call.respond(HttpStatusCode.NoContent)
-            else
-                call.respond(HttpStatusCode.NotFound, "Không tìm thấy user")
+            if (id == null) {
+                call.respondBase(
+                    HttpStatusCode.BadRequest,
+                    "ID không hợp lệ",
+                    Unit
+                )
+                return@delete
+            }
+            if (repo.delete(id)) {
+                call.respondBase(
+                    HttpStatusCode.NoContent,
+                    "Xóa thành công",
+                    Unit
+                )
+            } else {
+                call.respondBase(
+                    HttpStatusCode.NotFound,
+                    "Không tìm thấy user với id = $id",
+                    Unit
+                )
+            }
         }
     }
+}
+
+
+suspend inline fun <reified T> ApplicationCall.respondBase(
+    status: HttpStatusCode,
+    message: String,
+    data: T? = null
+) {
+    respond(
+        status,
+        BaseResponse(
+            code = status.value,
+            message = message,
+            data = data
+        )
+    )
 }
